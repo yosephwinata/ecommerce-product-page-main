05e113561c3263e9a907057e5053f11a8627912c
yosephwinata <yoseph.winata@gmail.com>
2024-01-19 19:43:02 +0700

    Create cart popover ui

---
+++ src/components/common/CartPopover.jsx
@@ -0,0 +1,50 @@
+import TrashSvgIcon from "/src/components/common/svg/TrashSvgIcon.jsx";
+
+const CartPopover = () => {
+  return (
+    <div className="absolute -bottom-2 left-1/2 z-50 w-[96%] -translate-x-1/2 translate-y-full overflow-hidden rounded-[10px] bg-white xl:bottom-0 xl:left-auto xl:right-0 xl:w-[22.5rem] xl:translate-x-16 xl:translate-y-[90%]">
+      <p className="border-b border-very-light-gray px-6 pb-[1.6875rem] pt-6 text-[1rem] font-bold">
+        Cart
+      </p>
+      <div className="justify-between px-6 pb-8 pt-6">
+        <div className="flex flex-col gap-3">
+          <CartItem />
+        </div>
+        <a
+          href="#"
+          className="mt-6 block rounded-[10px] bg-orange py-5 text-center text-[1rem] font-bold text-white"
+        >
+          Checkout
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const CartItem = () => {
+  return (
+    <div className="flex items-center">
+      <img
+        src="/src/assets/images/image-product-1-thumbnail.jpg"
+        alt="Product thumbnail"
+        className="w-[3.125rem] rounded"
+      />
+      <div className="ml-4">
+        <p className="text-[1rem] text-dark-grayish-blue">
+          Fall Limited Edition Sneakers
+        </p>
+        <div>
+          <span className="text-[1rem] text-dark-grayish-blue">$125.00 </span>
+          <span className="text-[1rem] text-dark-grayish-blue">x </span>
+          <span className="text-[1rem] text-dark-grayish-blue">3 </span>
+          <span className="text-[1rem] font-bold">$375.00</span>
+        </div>
+      </div>
+      <div className="ml-auto">
+        <TrashSvgIcon />
+      </div>
+    </div>
+  );
+};
+
+export default CartPopover;
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,10 +2,11 @@ import logo from "../../assets/images/logo.svg";
 import menuIcon from "../../assets/images/icon-menu.svg";
 import avatar from "../../assets/images/image-avatar.png";
 import NavBar from "./NavBar";
+import CartPopover from "./CartPopover";
 
 const Header = ({ onMenuButtonClick }) => {
   return (
-    <header className="xl:border-very-light-gray flex items-center px-6 pb-6 pt-[1.1875rem] xl:mx-auto xl:h-28 xl:w-[70rem] xl:border-b xl:px-0 xl:pb-0 xl:pt-0">
+    <header className="relative flex items-center px-6 pb-6 pt-[1.1875rem] xl:mx-auto xl:h-28 xl:w-[70rem] xl:border-b xl:border-very-light-gray xl:px-0 xl:pb-0 xl:pt-0">
       <img
         src={menuIcon}
         alt="Menu icon"
@@ -20,7 +21,7 @@ const Header = ({ onMenuButtonClick }) => {
         width="22"
         height="20"
         xmlns="http://www.w3.org/2000/svg"
-        className="hover:text-black ml-auto cursor-pointer text-dark-grayish-blue"
+        className="ml-auto cursor-pointer text-dark-grayish-blue hover:text-black"
       >
         <path
           d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
@@ -31,8 +32,9 @@ const Header = ({ onMenuButtonClick }) => {
       <img
         src={avatar}
         alt="User avatar"
-        className="border-transparent ml-5 w-6 cursor-pointer rounded-full border-2 hover:border-orange xl:ml-[2.875rem] xl:w-[3.125rem]"
+        className="ml-5 w-6 cursor-pointer rounded-full border-2 border-transparent hover:border-orange xl:ml-[2.875rem] xl:w-[3.125rem]"
       />
+      <CartPopover />
     </header>
   );
 };
